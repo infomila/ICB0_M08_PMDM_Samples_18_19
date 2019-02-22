6356713f8b27a4d5caced9c059be358d938af4f3
Bernat <bernat.orellana@gmail.com>
2019-02-22 19:35:35 +0100

    Muntatge amb fragments en funcionament. Manca pulir temes...

--- UF1/6_Fragments/app/src/main/java/net/iesmila/app6_fragments/MainActivity.java
+++ UF1/6_Fragments/app/src/main/java/net/iesmila/app6_fragments/MainActivity.java
@@ -1,12 +1,14 @@
 package net.iesmila.app6_fragments;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import net.iesmila.app6_fragments.dummy.DummyContent;
 
 public class MainActivity extends AppCompatActivity implements
-                                        FragmentItemList.OnListFragmentInteractionListener {
+                                        FragmentItemList.OnListFragmentInteractionListener,
+                                        FragmentDetail.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,36 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public void onListItemClick(DummyContent.DummyItem item) {
+        if(findViewById(R.id.frmDetail)!=null) {
+
+            // Estic en Landscape
+            //------------------------------
+
+            // Creem un fragment de tipus FragmentDetail i l'enxufem a frmDetail
+            // a) Creació del fragment
+            FragmentDetail frag = FragmentDetail.newInstance(item.id);
+            // b) El col·loquem a l'activity
+            getSupportFragmentManager().
+                    beginTransaction().
+                    replace(R.id.frmDetail,frag).
+                    commit();
+        }
+         else {
+            // Estic en portrait
+            // Crearem un intent per obrir el DetailActivity
+            Intent intent = new Intent(this,  DetailActivity.class );
+            intent.putExtra(DetailActivity.PARAM_ID, item.id );
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onItemChanged(String id) {
+
+    }
+
+    @Override
+    public void onItemDeleted(String id) {
 
     }
 }
